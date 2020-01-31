d58cbcb9933e45d2ad55621d1bb76a6373e139dd
gaintech <gainifes@gmail.com>
2020-01-31 23:20:38 +0000

    Insersao do launch do amcl, ajustes dos parametros do move_base

--- src/reset_odom.py
+++ src/reset_odom.py
@@ -6,19 +6,19 @@ import struct
 porta = 3003
 ipmyrio = '192.168.1.100'
 
-try:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (ipmyrio, porta)
-    client.connect(server_address)
+# try:
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (ipmyrio, porta)
+client.connect(server_address)
 
-    rospy.init_node('myrio_reset_odom', anonymous=False)
-    rospy.loginfo("Resetando Odometria ...")
-    data = client.recv(4)
-    codigo = struct.unpack('>i', data)[0]
-    if codigo:
-        rospy.loginfo("Odometria resetada")
-    else:
-        rospy.logerr("ERRO AO RESETAR A ODOMETRIA")
+rospy.init_node('myrio_reset_odom', anonymous=False)
+rospy.loginfo("Resetando Odometria ...")
+data = client.recv(4)
+codigo = struct.unpack('>i', data)[0]
+if codigo:
+    rospy.loginfo("Odometria resetada")
+else:
+    rospy.logerr("ERRO AO RESETAR A ODOMETRIA")
 
-except (KeyboardInterrupt,SystemExit,socket.error):
-    client.close()
+# except (KeyboardInterrupt,SystemExit,socket.error):
+#     client.close()
